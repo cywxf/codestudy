af0d7e8860ac11df4acb539f83671262deefbf11
XuQiao <qixu@cern.ch>
2014-11-13 10:18:05 -0600

    file inventory

---
+++ python/test.py
@@ -0,0 +1,41 @@
+import numpy as np
+import random
+wokp=[0]*1000
+def make1Step(oldList):
+    #newList = oldList.copy()
+    newList = list(oldList)
+    for i in range(len(oldList)):
+        flag = random.random()
+        if flag > 0.5:
+            newList[i] = oldList[i] + 1
+        else:
+            newList[i] = oldList[i] - 1
+    return newList
+def congen(shock):
+    gen=[100]*len(shock)
+    for i in range(len(shock)-1):
+        gen[i+1]=gen[i]+shock[i]
+    return gen
+
+def annatrans(semidat):
+    trans=[0]*(500)
+    for i in range(len(trans)):
+        trans[i]=semidat[i*2-1]+semidat[i*2]
+    return trans
+def delta(yeardat):
+    change=[0]*(len(yeardat)-1)
+    for i in range(len(change)):
+        change[i]=yeardat[i+1]-yeardat[i]
+    return change
+c1=make1Step(wokp)
+csy=congen(c1)
+cy=annatrans(csy)
+deltacy=np.array(delta(cy))
+y1=make1Step(wokp)
+ysy=congen(y1)
+yy=annatrans(ysy)
+deltayy=np.array(delta(yy))
+A=np.vstack([deltayy,np.ones(len(deltayy))]).T
+m=np.linalg.lstsq(A,deltacy)[0]
+m
+print m
