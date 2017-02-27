6a69ea84266369e4387ea554c4145235c739b50d
XuQiao <xu.qiao93@gmail.com>
2017-02-27 12:04:41 -0500

    learn R LM

--- C/beauty/jiangshuai.C
+++ C/beauty/jiangshuai.C
@@ -5,5 +5,11 @@ using namespace std;
 int main(){
 //only one variable is allowed
 //A: d10 - f8, B: d1 - f3
-for(int pos = 
+
+    cout << "A\t" << "B" << endl; 
+    for(int pos = 0; pos < 81; pos ++) {
+        if (pos / 3 / 3 / 3 % 3== pos / 3 % 3) continue;
+        cout << char(pos / 3 / 3 / 3 % 3 + 'd')  << pos / 3 / 3 % 3 + 8 << "\t" <<  
+                char(pos / 3 % 3 + 'd')  << pos % 3 + 1 << endl; 
+    }
 }
