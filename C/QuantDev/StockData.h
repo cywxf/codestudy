62c9ab38dd6632c90c1290cee38c7748fb82a096
XuQiao <xu.qiao93@gmail.com>
2017-04-25 22:49:13 -0400

    .gitignore is now working

--- C/QuantDev/StockData.h
+++ C/QuantDev/StockData.h
@@ -3,31 +3,20 @@
 #include <string>
 #include <iostream>
 
-struct Time{
-    int year;
-    int month;
-    int date;
-    int hour;
-    int minute;
-    int second;
-    int micros;
-};
-
-
+//Simply capsulation to store the stock data tick information
 class stock_data{
     public:
-        stock_data(){};
+        stock_data(const double &_time, const double &_price, const std::string &_exchange);
         ~stock_data(){};
         stock_data(const std::string& t);
-        int get_diff(const stock_data& d);
-        Time get_time() const{ return time;}
-        float get_price() const{ return price;}
+        double get_time() const{ return time;}
+        double get_price() const{ return price;}
         std::string get_exchange() const{ return exchange;}
         
     private:
-        Time time;
+        double time;
         std::string ticker;
-        float price;
+        double price;
         float size;
         std::string exchange;
 };
