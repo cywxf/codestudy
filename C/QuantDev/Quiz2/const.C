2d8354c412f7e448544b074efa7c3cb13f334257
XuQiao <xu.qiao93@gmail.com>
2017-06-11 16:21:55 -0400

    2

--- C/QuantDev/Quiz2/const.C
+++ C/QuantDev/Quiz2/const.C
@@ -4,7 +4,7 @@ using namespace std;
 //const int* const fun: the function returns a const pointer to const int
 //function doesn' change input
 
-const int* const fun(const int* const&p){
+const int* const fun(const int* const&p) const{
     const int *pt;
     pt = p + 1;
     return pt;
